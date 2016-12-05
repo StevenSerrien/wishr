069c23dbbf930bb0942c7881359e3be6f61cff18
StevenSerrien <steven.serrien@student.kdg.be>
2016-12-05 15:33:40 +0100

    Routes + templates in place

--- wishr-app/www/js/app.js
+++ wishr-app/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('wishr', ['ionic', 'ngMockE2E'])
+angular.module('wishr', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,4 +21,52 @@ angular.module('wishr', ['ionic', 'ngMockE2E'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+    })
+    .state('main', {
+        url: '/',
+        abstract: true,
+        templateUrl: 'templates/main.html'
+    })
+    .state('main.dash', {
+        url: 'main/dash',
+        views: {
+            'dash-tab': {
+                templateUrl: 'templates/dashboard.html',
+                controller: 'DashCtrl'
+            }
+        }
+    })
+    .state('main.public', {
+        url: 'main/public',
+        views: {
+            'public-tab': {
+                templateUrl: 'templates/public.html'
+            }
+        }
+    });
+    // .state('main.admin', {
+    //     url: 'main/admin',
+    //     views: {
+    //         'admin-tab': {
+    //             templateUrl: 'templates/admin.html'
+    //         }
+    //     },
+    //     data: {
+    //         authorizedRoles: [USER_ROLES.admin]
+    //     }
+    // });
+
+    // Thanks to Ben Noblet!
+    $urlRouterProvider.otherwise(function ($injector, $location) {
+        var $state = $injector.get("$state");
+        $state.go("login");
+    });
 })
