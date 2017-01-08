2849735441b7ae33420d91ba143312055c311f0d
StevenSerrien <steven.serrien@student.kdg.be>
2017-01-08 02:08:44 +0100

    dno why my code works..

--- wishr-website/src/app/components/dashboard/dashboard.js
+++ wishr-website/src/app/components/dashboard/dashboard.js
@@ -1,17 +1,23 @@
 function dashboardController($scope, $log, $rootScope, $timeout, wish) {
 $scope.user = {};
+$scope.wishlists = {};
 
+$scope.getWishlists = function () {
+  wish.getAllWishlists().then(function (results) {
+    $scope.wishlists = results;
+    $log.log($scope.wishlists);
+  });
+};
+$scope.getWishlists();
 init();
+
 function init() {
   $timeout(function () {
     $scope.user = angular.fromJson(localStorage.user);
-    wish.getAllWishlistsFromCurrentUser();
     $log.log($scope.user);
+    // $log.log($scope.wishlists);
   }, 100);
-
-
 }
-
 }
 module.exports = {
   templateUrl: 'app/components/dashboard/dashboard.html',
--- wishr-website/src/app/services/WishlistService.js
+++ wishr-website/src/app/services/WishlistService.js
@@ -1,7 +1,7 @@
-function WishlistService($http, $log, $rootScope) {
+function WishlistService($http, $log, $rootScope, $q) {
   var service = this;
 
-  service.getAllWishlistsFromCurrentUser = function (callback) {
+  service.getAllWishlistsFromCurrentUser = function () {
     if (localStorage.token) {
       $http({
         method: "GET",
@@ -11,11 +11,35 @@ function WishlistService($http, $log, $rootScope) {
         },
         url: $rootScope.BASE_URL + "/wishlist"
       }).then(function mySucces(response) {
-        $log.log(response);
+        $log.log(response.data);
+        return angular.toJson(response.data);
       }, function myError(response) {
         $log.log(response);
       });
     }
+  };
+  var results;
+  service.getAllWishlists = function () {
+    if (!results) {
+    // Create deferred object
+    var deferred = $q.defer();
+    $http({
+      method: 'GET',
+      headers: {
+        'Content-Type': undefined,
+        'Authorization': 'Bearer ' + localStorage.token
+      },
+      url: $rootScope.BASE_URL + "/wishlist"
+    }).then(function successCallback(response) {
+      results = response.data;
+      deferred.resolve(results);
+    }, function errorCallback(error) {
+      results = error;
+      deferred.reject(error);
+    });
+    results = deferred.promise;
+    }
+    return $q.when(results);
   };
 }
 
