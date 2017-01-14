b4a57f73663d67d27795ad8878ae30914b4555bf
StevenSerrien <steven.serrien@student.kdg.be>
2017-01-14 19:45:48 +0100

    show wishlist items

--- wishr-website/src/app/components/wishlist/wishlist.js
+++ wishr-website/src/app/components/wishlist/wishlist.js
@@ -4,11 +4,17 @@ function wishlistController(wish, $scope, $log, $stateParams) {
   $log.log($stateParams.wishlistId);
   $scope.items = {};
   // $scope.items = wish.getItemsOnWishlist($stateParams.wishlistId);
-  // $log.log($scope.items);
+  // $log.log($scope.items);  var rand = 0;
+  $scope.getRandomSpan = function (max, min) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+  };
 
   $scope.getItemsOnWishlist = function () {
     wish.getItemsOnWishlist($stateParams.wishlistId).then(function (results) {
-      $scope.items = results;
+      $scope.items = results.items;
+      for (var i = 0; i < $scope.items.length; i++) {
+        $scope.items[i].colSpan = $scope.getRandomSpan(6, 6);
+      }
       $log.log($scope.items);
     });
   };
