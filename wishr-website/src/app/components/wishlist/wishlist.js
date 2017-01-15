28d60d44bf6f3e89f7c3b39b08d9adbd25355c07
StevenSerrien <steven.serrien@student.kdg.be>
2017-01-15 02:01:49 +0100

    added a check to see if wishlist owner is the same as the current logged user for an add button

--- wishr-website/src/app/components/wishlist/wishlist.js
+++ wishr-website/src/app/components/wishlist/wishlist.js
@@ -1,10 +1,13 @@
-function wishlistController(wish, $scope, $log, $stateParams) {
+function wishlistController(wish, $scope, $log, $stateParams, auth) {
 
   this.text = 'My brand new component!';
   $log.log($stateParams.wishlistId);
   $scope.items = {};
   $scope.owner = {};
   $scope.wishlist = {};
+  $scope.sameUser = false;
+  $scope.currentUser = angular.fromJson(localStorage.user);
+  // $log.log($scope.currentUser);
   // $scope.items = wish.getItemsOnWishlist($stateParams.wishlistId);
   // $log.log($scope.items);  var rand = 0;
   $scope.getRandomSpan = function (max, min) {
@@ -13,20 +16,24 @@ function wishlistController(wish, $scope, $log, $stateParams) {
 
   $scope.getItemsOnWishlist = function () {
     wish.getItemsOnWishlist($stateParams.wishlistId).then(function (results) {
-      $log.log(results);
+      // $log.log(results);
       $scope.wishlist = results[1];
       $scope.items = results[2];
       $scope.owner = results[0][0];
       $log.log($scope.owner);
-      $log.log($scope.wishlist);
-      $log.log($scope.items);
+      $log.log($scope.currentUser);
+      if ($scope.owner.id === $scope.currentUser.id) {
+        $log.log('This is the current users list');
+        $scope.sameUser = true;
+      }
+      // $log.log($scope.wishlist);
+      // $log.log($scope.items);
       for (var i = 0; i < $scope.items.length; i++) {
         $scope.items[i].colSpan = $scope.getRandomSpan(6, 6);
       }
       // $log.log($scope.items);
     });
   };
-
   $scope.getItemsOnWishlist();
 }
 
